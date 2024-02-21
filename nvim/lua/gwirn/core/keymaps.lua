25de8ba2193169bc91362ba27587657a5ada15eb
gwirn <71886945+gwirn@users.noreply.github.com>
2024-02-21 20:52:31 +0100

    set alternate file shortcut to nop

--- nvim/lua/gwirn/core/keymaps.lua
+++ nvim/lua/gwirn/core/keymaps.lua
@@ -25,6 +25,7 @@ vim.keymap.set("t", "<leader><ESC>", "<C-\\><C-N>", { desc = "back to normal mod
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "<C-^>", "<Nop>")
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
