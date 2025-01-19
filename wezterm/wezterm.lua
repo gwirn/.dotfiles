fcbd806bc15d3b1fed8bbf302db8664ba8c43e9d
gwirn <71886945+gwirn@users.noreply.github.com>
2025-01-19 09:07:48 +0100

    cleaner terminal

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,7 +9,7 @@ end
 
 -- config.color_scheme = "s3r0 modified (terminal.sexy)"
 
-config.font_size = 13.0
+config.font_size = 16.0
 config.window_decorations = "RESIZE"
 config.default_cursor_style = "SteadyBar"
 
@@ -18,27 +18,15 @@ config.font = wezterm.font_with_fallback({
 	"JetBrains Mono",
 	"Apple Color Emoji",
 })
-config.mouse_bindings = {
-	-- Bind 'Up' event of CTRL-Click to open hyperlinks
-	{
-		event = { Up = { streak = 1, button = "Left" } },
-		mods = "CTRL",
-		action = act.OpenLinkAtMouseCursor,
-	},
-	-- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
-	{
-		event = { Down = { streak = 1, button = "Left" } },
-		mods = "CTRL",
-		action = act.Nop,
-	},
-}
 local theme = require("colors/misty")
 config.colors = theme
-config.foreground_text_hsb = {
-	hue = 1.0,
-	saturation = 1.0,
-	brightness = 1.0,
+config.inactive_pane_hsb = {
+	saturation = 0.9,
+	brightness = 0.8,
 }
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.9
+
 -- config.colors = {
 -- 	cursor_bg = "orange",
 -- }
@@ -46,7 +34,7 @@ config.foreground_text_hsb = {
 config.keys = {
 	-- This will create a new split and run your default program inside it
 	{
-		key = "t",
+		key = "s",
 		mods = "CTRL|ALT|SHIFT|WIN",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
@@ -61,10 +49,29 @@ config.keys = {
 		action = wezterm.action({ ActivatePaneDirection = "Prev" }),
 	},
 	{
-		key = "w",
+		key = "t",
+		mods = "CTRL|ALT|SHIFT|WIN",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+
+	{
+		key = "z",
 		mods = "CTRL|ALT|SHIFT|WIN",
 		action = wezterm.action.ActivateTabRelative(1),
 	},
 }
-
+config.mouse_bindings = {
+	-- Bind 'Up' event of CTRL-Click to open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.OpenLinkAtMouseCursor,
+	},
+	-- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+	{
+		event = { Down = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.Nop,
+	},
+}
 return config
